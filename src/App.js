2664cdbe67489399f0ceeaf32a837991405b0b1b
HowardHarrison <peterthureain@gmail.com>
2023-08-03 18:13:39 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes, Link, useLocation, NavLink } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import { BookLayout } from './BookLayout';
+import { BookRoutes } from './BookRoutes';
 
 function App() {
+  const location = useLocation()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+     
+       <nav>
+      <ul>
+        <li>
+          <NavLink to='/' state='Hi'>Home</NavLink>
+        </li>
+        <li>
+          <Link to='/books'>Books</Link>
+        </li>
+      </ul>
+    </nav>
+    {location.state}
+    <Routes>
+     <Route path='/' element={<Home/>}/> 
+     <Route path='/books/*' element={<BookRoutes/>}/>
+     <Route path='*' element={<NotFound/>}/>
+     {/*<Route path='/books' element={<BookLayout/>}>
+      <Route index element={<Book/>}/>
+      <Route path=':id' element={<BookList/>}/>
+      <Route path='new' element={<NewBook/>}/>
+  </Route>*/}
+
+     {/*<Route path='/books' element={<Book/>} />
+     <Route path='/books/:id' element={<BookList/>} />
+  <Route path='/books/new' element={<NewBook/>} /> */}
+   
+   </Routes> 
+   </div>
   );
 }
 
---
+++ src/pages/Book.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+export function Book() {
+    return (
+     <>
+      <h1>Book</h1>
+     </>
+    ) 
+}
